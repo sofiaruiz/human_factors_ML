0c5272d65dda463f56799be663a9e138ea22098c
sofia <sruiz13@lion.lmu.edu>
2019-04-30 17:00:31 -0700

    adding main.js to use with mturk project

---
+++ mturk_app/main.js
@@ -0,0 +1,112 @@
+var articles = [
+    article1 = {
+        title: "Trump Fed pick Stephen Moore: Biggest problem to the US economy is declining 'male earnings'",
+        text : `Washington Stephen Moore, President Donald Trump's pick for the Federal Reserve, said Tuesday the biggest problem to the US economy over the years is the decline in "male earnings." 
+        "The biggest problem I see in the economy over the last 25 years is what has happened to male earnings -- for black males and white males as well. They've been declining and that is, I think, a big problem," Moore said during an interview with CNBC's "Squawk Box." 
+        He continued: "Look I want everybody's wages to rise, of course, but you know, people are talking about women's earnings -- they've risen. The problem, actually, has been the steady decline in male earnings, and I think we should pay attention to that because I think that has very negative consequences for the economy and for society." 
+        ￼
+        Data collected by the Department of Labor shows median earnings have increased more rapidly for women than men over the last 25 years, but wages have gone up for both those groups over that time frame. Much of the political conversation in recent years has been about the disparity in pay for men and women doing the same or similar work, rather than generally.
+        
+                
+        Moore, a conservative economic commentator and former Trump campaign adviser, has been under scrutiny over columns he wrote decades ago dismissing women's participation in professional sports and specifically attacking women athletes over pay, first surfaced last week by CNN's KFile. 
+        As a writer for the conservative National Review, Moore asserted that women should be banned from refereeing, announcing or beer vending at men's college basketball games. He also criticized female athletes who advocated for pay equality, complaining that they wanted "equal pay for inferior work." 
+        In an email to CNN's KFILE last week, Moore said, "This was a spoof. I have a sense of humor."
+        During a radio interview last Tuesday, Moore compared himself to Supreme Court Justice Brett Kavanaugh, whose confirmation last year was marked by allegations he had committed sexual assault as a teenager. In one interview he said that reporters covering him at various news outlets -- including CNN -- are "pulling a Kavanaugh against me."
+        
+                
+        The White House is sending conflicting signals on Moore's nomination, with press secretary Sarah Sanders saying the White House is reviewing the writings resurfaced by CNN's KFILE, and National Economic Council director Larry Kudlow saying hours later that their nominee still has their full support. 
+        Meanwhile, Moore said last week he would back out of the process if he becomes a political "liability," according to the Wall Street Journal. `,
+        source : "CNN"
+    } , 
+    article2 = {
+        title : "Trump sues Deutsche Bank and Capital One over Democrat subpoenas",
+        text : `US President Donald Trump has sued two banks in a bid to stop them handing over his financial records to Congress.
+        The lawsuit against Deutsche Bank and Capital One was filed after Democrat-led committees issued subpoenas for information on his finances.
+        His three eldest children and the Trump Organisation joined the lawsuit, which argues there are no legitimate grounds for investigating his business affairs.
+        Leading Democrats said they would not be deterred by the blocking attempt.
+        A Deutsche Bank spokeswoman told the New York Times: "We remain committed to providing appropriate information to all authorized investigations"
+        There was no immediate comment from Capital One.
+        What does the Trump lawsuit say?
+        The lawsuit was filed in New York on Monday and aims to block the banks from complying with the subpoenas issued by the Democrat-controlled Congress, which is investigating the Trump family's financial dealings.
+        Both banks are involved with Mr Trump's real estate projects.
+        The filing says the subpoenas "have no legitimate or lawful purpose".
+        "[They] were issued to harass President Donald J. Trump, to rummage through every aspect of his personal finances, his businesses, and the private information of the president and his family," it alleges.
+        It adds: "No grounds exist to establish any purpose other than a political one."
+        The filing also says that Democrats are hoping "they will stumble upon something they can expose publicly and use as a political tool against the President".
+        Why were the banks subpoenaed?
+        The House Intelligence and Financial Services Committees issued subpoenas to Germany's Deutsche Bank, which is one of the Trump Organisation's major lenders, and a number of other financial institutions earlier this month.
+        The committees are reportedly seeking documents and materials relating to the bank's longstanding relationship with Mr Trump and his family.
+        Representative Adam Schiff, the Democratic chairman of the House Intelligence Committee, said the subpoenas were part of an investigation "into allegations of potential foreign influence on the US political process".
+        Mr Schiff and fellow Democrat Maxine Waters, the chairwoman of the Financial Services Commission, described the lawsuit as "meritless".
+        "[It shows] the depths to which President Trump will go to obstruct Congress's constitutional oversight authority," they said in a joint statement.
+        "He will find that Congress will not be deterred from carrying out its constitutional responsibilities," it adds.
+        In 2017, the bank rejected requests by Democrats in the House of Representatives to provide details of the president's finances, citing privacy laws.
+        That year it was also reported that special counsel Robert Mueller, who led the investigation into alleged collusion between the Trump presidential campaign and Russia, had ordered Deutsche Bank to provide records of Mr Trump's accounts.
+        ` , 
+        source : "BBC"
+    } , 
+    article3 = {
+        title : "Right-wing activists accused of orchestrating false sexual assault allegations against Pete Buttigieg",
+        text : `A pair of notorious right-wing activists are facing accusations of orchestrating a plan for a man to falsely claim he was sexually assaulted by Democratic presidential candidate Pete Buttigieg.
+
+        The two activists – lobbyist Jack Burkman and blogger Jacob Wohl – have been involved in pushing conspiracy theories and inflammatory accusations against others in the past.
+        
+        Everything began Monday when a post surfaced on the website Medium from an author identifying himself as a 21-year-old named Hunter Kelly. The post accused Buttigieg of sexually assaulting him in February, and promised to reveal more details later.
+
+        But by the end of the day, the story began to unravel.
+        
+        First, the Daily Beast reported that a Republican source said Burkman and Wohl recently tried to convince him to falsely accuse Buttigieg, who is gay, of inappropriate sexual behavior.
+        
+        Then, a man named Hunter Kelly – the name of the supposed author of the Medium post accusing Buttigieg of assault – wrote on Facebook that he was “not sexually assaulted and would never falsely accuse anyone.”
+        He acknowledged being “approached by a political figure to come to DC to discuss political situations from the standpoint of a gay Republican.”
+
+        “When I arrived they discussed Peter Buttigieg and started talking about how they would be working a campaign against him,” Kelly wrote. “I went to bed and woke up to a fake Twitter @RealHunterKelly and an article that I in no way endorsed or wrote.”
+        
+        Kelly told The Daily Beast of the allegations that were posted on Medium under his name: “I was unaware this was happening.”
+        
+        At some point, the original Medium post was removed from the website.
+        
+        Burkman released a statement on Tuesday “vehemently denying the claims” he was involved in fabricating the story, claiming he was approached by Kelly.
+        “This weekend my firm was approached by a young man who had a disturbing story about an assault he experienced at the hands of current presidential candidate, Pete Buttigieg,” Burkman said. “We began working with him on sharing his painful story and received a signed statement from him attesting to his account.”
+
+        In a separate statement to Fox News, Wohl said, "Mr. Kelly approached Jack Burkman and I with a story to tell. The very first thing that he did was sign an attestation detailing his accusation. Mr. Kelly had full control over any and all public disclosures that were made. Upon being bullied by the mainstream media, in the form of hundreds of phone calls to their places of work, his family demanded that he claim that we set him up. We know that these things are very difficult for young people like Mr. Kelly and we empathize with him."
+        
+        Burkman released a photo of what he claims is a signed statement from Kelly detailing his accusations. He also released a photo of a man he says is Kelly holding an identification card of himself.
+        
+        But talking to the website the Advocate, Kelly claims he was coerced into signing the form and taking the photo.
+        
+        "They basically forced me to sign that and take that photo," Kelly said, according to the publication. "I had no say in either. In the photo you can clearly see I had been crying."
+        
+        For his part, Buttigieg, campaigning in New York on Monday, said the accusations are false.
+        
+        “I'm sure it's not the first time somebody is going to make something up about me,” Buttigieg told reporters. “It's not going to throw us. Politics can be ugly sometimes but you have to face that when you're in presidential politics.”
+        
+        In October, Burkman and Wohl were earlier accused of being behind a plot to pay women to make false accusations of sexual harassment against Special Counsel Robert Mueller. The special counsel’s office said they referred the report about Burkman to the FBI.
+        
+        “When we learned last week of allegations that women were offered money to make false claims about the Special Counsel, we immediately referred the matter to the FBI for investigation,” special counsel spokesman Peter Carr said in a statement at the time.` , 
+        source : "Fox News"
+    } 
+]; 
+
+var allSources = ["CNN", "Fox News", "BBC"];
+
+function getArticle() {
+    let randomIndex =  Math.floor(Math.random() * Math.floor(3));
+    return articles[randomIndex];
+}
+
+function decideSource(article) {
+    let coin = Math.floor(Math.random() * Math.floor(2));
+    if (coin == 1) {
+        return article;
+    } else {
+        console.log("source should be:" + article.source);
+        article.source = allSources[Math.floor(Math.random() * Math.floor(3))];
+        console.log("source was changed to:" + article.source);
+        return article;
+    }
+}
+
+
+
+
